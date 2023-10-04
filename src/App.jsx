dc3b02cf8a9a3565d3856c6f54f95d154bc091f6
NagaMaruthi <nagamaruthi567@gmail.com>
2023-10-04 16:23:35 +0530

    project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Header from "./shared/header";
+import { Outlet } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { firebaseConfig } from './shared/firebase';
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+ export default App;
+
+// import React from "react";
+// import Header from "./shared/header";
+// import { Outlet } from "react-router-dom";
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { firebaseConfig } from './shared/firebase';
+// export const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// function App() {
+//   return (<div>
+//     <Header></Header>
+//     <Outlet></Outlet>
+//     </div>
+//   );
+// }
+
+
---
+++ src/shared/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXy2pPZEQEe014wSvLQSBCI5xiORB6Rho",
+  authDomain: "hospital-app-69a13.firebaseapp.com",
+  projectId: "hospital-app-69a13",
+  storageBucket: "hospital-app-69a13.appspot.com",
+  messagingSenderId: "774095168753",
+  appId: "1:774095168753:web:c2f442196eab19c51eb550",
+  measurementId: "G-L5JV8GXM5E"
+};
